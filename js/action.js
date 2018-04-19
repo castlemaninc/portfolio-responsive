45d69bcf0a60962591f2751843beacba069c9def
Bryan Castleman <castlemaninc@gmail.com>
2018-04-19 07:21:18 -0700

    changed selector in jquery to fix responsive nav footer links

--- js/action.js
+++ js/action.js
@@ -1,7 +1,7 @@
 $( document ).ready(function() {
 
 	// Smooth scrolling to the top of the webpage when 'Back to Top link is clicked'
-	$('footer a').click(function(e) {
+	$('footer h4 a').click(function(e) {
 	  	console.log('footer a clicked');
 	    
 	    e.preventDefault();
