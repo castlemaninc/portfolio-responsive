7727d28bbb7e6e9d3ab99275fb28b9be6c77a832
Bryan Castleman <castlemaninc@gmail.com>
2018-04-18 21:46:18 -0700

    adding initial files

---
+++ js/action.js
@@ -0,0 +1,32 @@
+$( document ).ready(function() {
+
+	// Smooth scrolling to the top of the webpage when 'Back to Top link is clicked'
+	$('footer a').click(function(e) {
+	  	console.log('footer a clicked');
+	    
+	    e.preventDefault();
+	    
+	    $('body,html').animate({
+	      scrollTop: $(this.hash).offset().top
+	    }, 700 );
+  	});
+
+  	$('.bars-click-area').on('click', function(){
+  		$('.responsive-menu').show();
+  		$('.site-grid').hide();
+  	});
+
+  	$('.times-click-area').on('click', function(){
+  		$('.responsive-menu').hide();
+  		$('.site-grid').show();
+  	})
+
+  	$(window).resize(function(){
+        if($( window ).width() > 753) {
+        	$('.responsive-menu').hide();
+  			$('.site-grid').show();
+        }
+    });  	
+
+	console.log('ready');
+});
